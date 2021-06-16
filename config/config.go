4c2ef5baf96e4d5b87ccf18142a4c47e9a8af967
BuildTools <dominik.ochs@gmx.net>
2021-06-16 12:07:25 +0200

    add: config package

---
+++ config/config.go
@@ -0,0 +1,13 @@
+package config
+
+import (
+	"github.com/Project-Planner/backend/web"
+	"github.com/Project-Planner/backend/xmldb"
+)
+
+// Config of the project, embedding all sub-configs
+type Config struct {
+	// here go the config fields and embedded configs of other packages
+	web.ServerConfig
+	xmldb.DBConfig
+}
