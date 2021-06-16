99948e64d499cf3f5e35dd0f79794d8d6486dab1
BuildTools <dominik.ochs@gmx.net>
2021-06-16 12:08:55 +0200

    add: model package

---
+++ model/user.go
@@ -0,0 +1,7 @@
+package model
+
+// In model go all the structs that are required by both web and xmldb (or any db implementation)
+
+type User struct {
+	// User fields go here (possibly with XML tags)
+}
