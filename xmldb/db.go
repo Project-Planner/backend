f8276c711c22befa7c411883256852cf48f44139
BuildTools <dominik.ochs@gmx.net>
2021-06-16 12:11:01 +0200

    add: xml database implementation package

---
+++ xmldb/db.go
@@ -0,0 +1,9 @@
+package xmldb
+
+// The struct implementing the web.Database interface
+type database struct {
+	conf DBConfig
+}
+
+// Example implementation of a method
+// func (db database) GetUser(id string) (model.User, error) {}
