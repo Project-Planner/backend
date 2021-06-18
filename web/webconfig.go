9ad3fa26dcd371cf0d9a140275bd5909462028ec
BuildTools <dominik.ochs@gmx.net>
2021-06-18 12:22:21 +0200

    update: added config fields and yaml annotation

--- web/webconfig.go
+++ web/webconfig.go
@@ -3,7 +3,12 @@ package web
 // ServerConfig of the web web
 type ServerConfig struct {
 	// Here go the config fields that are relevant to the webserver
-	Port           int
-	StaticDir      string
-	AuthedPathName string
+	// Port for listening to http requests
+	Port int `yaml:"port"`
+	// StaticDir where the static web pages reside, that may be viewed by anyone
+	StaticDir string `yaml:"static_dir"`
+	// HTMLDir for other html files
+	HTMLDir string `yaml:"html_dir"`
+	// AuthedPathName - Path prefix of routes requiring authentication
+	AuthedPathName string `yaml:"authed_path_name"`
 }
