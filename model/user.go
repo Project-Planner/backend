67738833c7a6e1f0999adaed0d1366aa62e86b56
4kills <dominik.ochs@gmx.net>
2021-06-20 13:17:32 +0200

    update: added fields to user and tests for parsing

--- model/user.go
+++ model/user.go
@@ -1,7 +1,21 @@
 package model
 
+import "encoding/xml"
+
 // In model go all the structs that are required by both web and xmldb (or any db implementation)
 
 type User struct {
-	// User fields go here (possibly with XML tags)
+	XMLName xml.Name `xml:"user"`
+	Text    string   `xml:",chardata"`
+	Name    struct {
+		Text string `xml:",chardata"`
+		Val  string `xml:"val,attr"`
+	} `xml:"name"`
+	Items struct {
+		Text      string `xml:",chardata"`
+		Calendars []struct {
+			Text string `xml:",chardata"`
+			Href string `xml:"href,attr"`
+		} `xml:"calendar"`
+	} `xml:"items"`
 }
