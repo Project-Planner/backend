0d8e2fe7fc2e904ab83ff44649f839e32bbf73b7
BuildTools <dominik.ochs@gmx.net>
2021-07-07 12:51:41 +0200

    add: scaffold for adding adding, modifying and deleting appointments

---
+++ web/appointment.go
@@ -0,0 +1,15 @@
+package web
+
+import "net/http"
+
+func postAppointmentHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func putAppointmentHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteAppointmentHandler(w http.ResponseWriter, r *http.Request) {
+
+}
--- web/consts.go
+++ web/consts.go
@@ -7,6 +7,7 @@ const (
 	tokenIDStr    = "token_id"
 	userIDStr     = "user_id"
 	calendarIDStr = "calendar_id"
+	itemIDStr     = "item_id"
 	expiryStr     = "expiry"
 	authStr       = "auth"
 	jwtDuration   = time.Hour * 365 * 24
