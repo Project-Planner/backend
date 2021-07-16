6398dbf41435f4f4b30c5fdca8f774b79d31abc3
BuildTools <dominik.ochs@gmx.net>
2021-07-16 16:38:46 +0200

    add: stringer to permission

--- model/permission.go
+++ model/permission.go
@@ -9,6 +9,16 @@ const (
 	Owner
 )
 
+func (p Permission) String() string {
+	switch p {
+	case None: return "none"
+	case Read: return "view"
+	case Edit: return "edit"
+	case Owner: return "owner"
+	}
+	panic("permission: string: not implemented")
+}
+
 // CalendarPermissions takes a calendar c, and a user id and returns which permissions this user has for c.
 func CalendarPermissions(c Calendar, userID string) Permission {
 	if c.Owner.Val == userID {
