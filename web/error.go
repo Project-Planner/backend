987a37d362ef945b03449ee49291a2a232fed985
BuildTools <dominik.ochs@gmx.net>
2021-07-17 23:36:52 +0200

    update: made fields of error public

--- web/error.go
+++ web/error.go
@@ -12,8 +12,8 @@ func writeError(w http.ResponseWriter, msg string, code int) {
 	w.WriteHeader(code)
 
 	_ = errTemplate.Execute(w, struct {
-		detailedError string
-		code string
-		status string
-	}{detailedError: msg, code: fmt.Sprint(code), status: http.StatusText(code)})
+		DetailedError string
+		Code          string
+		Status        string
+	}{DetailedError: msg, Code: fmt.Sprint(code), Status: http.StatusText(code)})
 }
