36d4d8b188f43701c3a9398108a270b29c02720b
BuildTools <dominik.ochs@gmx.net>
2021-07-21 10:40:35 +0200

    refactor: restructured error.go

--- web/error.go
+++ web/error.go
@@ -9,16 +9,19 @@ import (
 var errTemplate *template.Template
 
 func writeError(w http.ResponseWriter, msg string, code int) {
-	w.WriteHeader(code)
-
 	if errTemplate == nil {
 		http.Error(w, msg, code)
 		return
 	}
 
-	_ = errTemplate.Execute(w, struct {
+	err := errTemplate.Execute(w, struct {
 		DetailedError string
 		Code          string
 		Status        string
 	}{DetailedError: msg, Code: fmt.Sprint(code), Status: http.StatusText(code)})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	w.WriteHeader(code)
 }
